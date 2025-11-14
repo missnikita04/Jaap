0bf5bbea2d0340a999a7adf98676fd99a5208b16
nikita chauhan <nikitachauhan3802@gmail.com>
2025-11-14 06:35:11 -0800

    -

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const API_URL = import.meta.env.VITE_API_URL;
 
 const SignupForm = () => {
+  const navigate = useNavigate();
   const [message, setMessage] = useState("");
   const [form, setFormData] = useState({
     username: "",
