d1f30cc574eb21a56fe403fc4c03e78029046fa4
nikita chauhan <nikitachauhan3802@gmail.com>
2025-11-17 00:17:24 -0800

    change style of carousel

--- Frontend/src/components/NavCarousel.jsx
+++ Frontend/src/components/NavCarousel.jsx
@@ -5,48 +5,61 @@ import "slick-carousel/slick/slick-theme.css";
 import { japData } from "../assets/assets";
 
 const NavCarousel = ({ onImageSelect }) => {
-const settings = {
-  dots: false,
-  infinite: true,
-  speed: 400, // faster speed
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  centerMode: true,
-  centerPadding: "0px",
-  initialSlide: 0,
-  responsive: [
-    {
-      breakpoint: 768,
+  const settings = {
+    dots:false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,           // desktop max slides
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    initialSlide: 0,
+    arrows: false,
+    centerMode: true,          // center active slide
+    centerPadding: "0px",
+    swipeToSlide: true,
+    accessibility: false,
+focusOnSelect: false,
+    responsive: [
+{
+      breakpoint: 1024,
       settings: {
-        slidesToShow: 2,
-        centerMode: true,
-        centerPadding: "0px",
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
       },
     },
     {
       breakpoint: 480,
       settings: {
-        slidesToShow: 1,
-        centerMode: true,
-        centerPadding: "0px",
+        slidesToShow: 3,
+        slidesToScroll: 1,
       },
-    },
+    },    
   ],
-};
+  };
+
   return (
     <div className="w-full bg-transparent pt-16 text-white overflow-x-hidden">
-      <div className="max-w-[99vw] mx-auto  pb-0 overflow-hidden">
+      <div className="max-w-[99vw] mx-auto  pt-1 pb-0 overflow-hidden">
         <Slider {...settings}>
           {japData.map((god, index) => (
             <div
               key={index}
-              className="flex flex-col items-center transform transition-transform duration-300 hover:scale-110"
+              className="flex flex-col items-center pt-4  transform transition-transform duration-300 hover:scale-110  z-20"
             >
               <img
                 src={god.src}
                 alt={god.name}
                 onClick={() => onImageSelect(god)}
-                className="object-cover rounded-full  w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-28 lg:h-28 border-white shadow-lg mx-auto transition duration-300 hover:scale-105 !important"
+                className="object-center object cover rounded-full  w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-28 lg:h-28 border-white shadow-lg mx-auto transition duration-300 hover:scale-105 !important"
               />
               <p className="mt-2 text-center text-white font-semibold text-xs sm:text-sm md:text-base lg:text-lg opacity-80 hover:opacity-100 transition-opacity duration-300 !important">
                 {god.name}
